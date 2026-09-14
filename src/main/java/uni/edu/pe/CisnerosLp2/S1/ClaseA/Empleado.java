a2ff9360700f8e740cd8266e2a767abf11d27a15
Jose2709 <luis_jose1978@hotmail.com>
2026-09-14 18:00:30 -0500

    Creando Carpeta de Tareas

---
+++ src/main/java/uni/edu/pe/CisnerosLp2/S1/ClaseA/Empleado.java
@@ -0,0 +1,10 @@
+package uni.edu.pe.CisnerosLp2.S1.ClaseA;
+
+public abstract class Empleado {
+    String nombre;
+
+    int calcularPago(int horas){
+        int pagoTotal = 1500 + horas*15
+    };
+
+}
